60590b5bc4bf5c5c1296c44d9708ca1c186831c5
unknown <oa075855@gmail.com>
2022-10-29 03:58:31 +0500

    commit

---
+++ js/questions.js
@@ -0,0 +1,70 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Html Stands For _________",
+    answer: "Hypertext markup language",
+    options: [
+      "Hyper Text Makeup Language",
+      "Html",
+      "Case Cading Style Sheet",
+      "Hypertext markup language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Css Stands For _________",
+    answer: "Casecading Style Sheet",
+    options: [
+      "Hyper Text Makeup Language",
+      "Ram",
+      "Java",
+      "Casecading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Js Stands For _________",
+    answer: "Java Script",
+    options: [
+      "Script",
+      "Java Style",
+      "Java Script",
+      "Script Src"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Dom Stands For _________",
+    answer: "Document Object Model",
+    options: [
+      "Css",
+      "Html",
+      "Document Object Model",
+      "Java"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Ram Stands For _________",
+    answer: "Random Acccess Memory",
+    options: [
+      "Random Acccess Memory",
+      "Read Only Memory",
+      "Dom",
+      "For Pc"
+    ]
+  },
+ 
+    {
+  numb: 6,
+    question: "Rom Stands For _________",
+  answer: "Read Only Memory",
+   options: [
+       "html",
+       "HTml",
+       "Read Only Memory",
+       "Hyper Text Markup Language"
+    ]
+  }
+]
